46297b558b2d2ac94280a0fd760273bea4f71be2
mintolges <mintolges14@gmail.com>
2024-11-16 16:47:03 +0200

    Update login.component.ts

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -15,7 +15,9 @@ import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 import { FormSubmitService } from '../Services/form-submit.service'
 import { ConfigurationService } from '../Services/configuration.service'
 import { BasketService } from '../Services/basket.service'
+import dotenv from 'dotenv';
 
+dotenv.config();
 library.add(faKey, faEye, faEyeSlash, faGoogle)
 
 const oauthProviderUrl = 'https://accounts.google.com/o/oauth2/v2/auth'
@@ -35,7 +37,7 @@ export class LoginComponent implements OnInit {
   public user: any
   public rememberMe: UntypedFormControl = new UntypedFormControl(false)
   public error: any
-  public clientId = '1005568560502-6hm16lef8oh46hr2d98vf2ohlnj4nfhq.apps.googleusercontent.com'
+  public clientId = process.env.GOOGLE_OAUTH_CLIENT_ID
   public oauthUnavailable: boolean = true
   public redirectUri: string = ''
   constructor (private readonly configurationService: ConfigurationService, private readonly userService: UserService, private readonly windowRefService: WindowRefService, private readonly cookieService: CookieService, private readonly router: Router, private readonly formSubmitService: FormSubmitService, private readonly basketService: BasketService, private readonly ngZone: NgZone) { }
