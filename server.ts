bfaa3dbde7eaaddec4b96f4c16768ce240ceff58
mintolges <mintolges14@gmail.com>
2024-11-16 16:10:38 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -2,6 +2,8 @@
  * Copyright (c) 2014-2024 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
+import * as dotenv from 'dotenv';
+dotenv.config();
 import dataErasure from './routes/dataErasure'
 import fs = require('fs')
 import { type Request, type Response, type NextFunction } from 'express'
