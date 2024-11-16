38e6e550fb61ac2b7ac52c4b18590649d0920ef8
mintolges <mintolges14@gmail.com>
2024-11-16 16:42:40 +0200

    Update last-login-ip.component.spec.ts

--- frontend/src/app/last-login-ip/last-login-ip.component.spec.ts
+++ frontend/src/app/last-login-ip/last-login-ip.component.spec.ts
@@ -7,6 +7,9 @@ import { type ComponentFixture, TestBed, waitForAsync } from '@angular/core/test
 import { LastLoginIpComponent } from './last-login-ip.component'
 import { MatCardModule } from '@angular/material/card'
 import { DomSanitizer } from '@angular/platform-browser'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 describe('LastLoginIpComponent', () => {
   let component: LastLoginIpComponent
@@ -47,13 +50,13 @@ describe('LastLoginIpComponent', () => {
   })
 
   xit('should set Last-Login IP from JWT as trusted HTML', () => { // FIXME Expected state seems to leak over from previous test case occasionally
-    localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Imxhc3RMb2dpbklwIjoiMS4yLjMuNCJ9fQ.RAkmdqwNypuOxv3SDjPO4xMKvd1CddKvDFYDBfUt3bg')
+    localStorage.setItem('token', process.env.TOKEN_HTML)
     component.ngOnInit()
     expect(sanitizer.bypassSecurityTrustHtml).toHaveBeenCalledWith('<small>1.2.3.4</small>')
   })
 
   xit('should not set Last-Login IP if none is present in JWT', () => { // FIXME Expected state seems to leak over from previous test case occasionally
-    localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7fX0.bVBhvll6IaeR3aUdoOeyR8YZe2S2DfhGAxTGfd9enLw')
+    localStorage.setItem('token', process.env.TOKEN_JWT)
     component.ngOnInit()
     expect(sanitizer.bypassSecurityTrustHtml).not.toHaveBeenCalled()
   })
